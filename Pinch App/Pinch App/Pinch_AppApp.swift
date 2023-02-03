1a4b91481be1f2cdb3f7bffdd5a8d0f7da490a5b
Yusril <yusrilchalif@gmail.com>
2023-02-03 08:56:12 +0700

    init

---
+++ Pinch App/Pinch App/Pinch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_AppApp.swift
+//  Pinch App
+//
+//  Created by Yusril on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
